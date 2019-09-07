9bd8c46a6ac0c5bfb4fb295a0f5af445048dfb3b
RiriPop34 <arielle.paiement@gmail.com>
2019-09-07 12:29:08 -0400

    panneau historique

--- src/views/vision/History.vue
+++ src/views/vision/History.vue
@@ -6,6 +6,20 @@
         <p>
           {{$t('history:content2')}}
         </p>
+        <v-expansion-panels>
+          <v-expansion-panel>
+            <v-expansion-panel-header>2013</v-expansion-panel-header>
+            <v-expansion-panel-content>
+              {{$t('history:panel1')}}
+            </v-expansion-panel-content>
+          </v-expansion-panel>
+          <v-expansion-panel>
+            <v-expansion-panel-header>2014</v-expansion-panel-header>
+            <v-expansion-panel-content>
+              {{$t('history:panel2')}}
+            </v-expansion-panel-content>
+          </v-expansion-panel>
+        </v-expansion-panels>
     </panel>
 </template>
 
@@ -17,12 +31,16 @@
             i18n.i18next.addResources('fr', 'history', {
                 title: 'Histoire',
                 content: "Lorem ipsum",
-                content2: "Appuyer sur tab va ajouter une nouvelle bulle"
+                content2: "Appuyer sur tab va ajouter une nouvelle bulle",
+                panel1: "Vincent rêve aux minds map",
+                panel2: "Vincent, maintenant Chenzo, code des mind map"
             });
             i18n.i18next.addResources('en', 'history', {
                 title: 'History',
                 content: "Lorem ipsum",
-                content2: "Pressing tab will add a new bubble"
+                content2: "Pressing tab will add a new bubble",
+                panel1: "Vincent dreams of minds map",
+                panel2: "Vincent, now Chenzo, codes mind map"
             });
             return {};
         }
